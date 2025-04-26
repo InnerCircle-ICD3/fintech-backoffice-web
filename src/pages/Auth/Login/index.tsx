23016f373c2ca0e928b131b594bf1d499f31de8c
CHOIYEJUN <dpwns108@gmail.com>
2025-04-26 11:30:41 +0900

    feat : 초기세팅
    - 초기세팅
    
    #main

---
+++ src/pages/Auth/Login/index.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+
+
+    return (
+        <>
+            로그인 페이지 Index
+        </>
+    )
+
+}
+
+export default Login
---
+++ src/router/RouterProvider.tsx
@@ -0,0 +1,30 @@
+import Main from "@/pages/Main";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import Auth from "@/pages/Auth";
+
+const CustomRouterProvider = () => {
+
+    const browserRouter = createBrowserRouter([
+        {
+            path: '',
+            element: <Main/>,
+            children: [
+                {
+                    path: 'auth',
+                    element: <Auth/>,
+                    children: [
+                        {
+                            index: true,
+                            element: <Navigate to={'login'}/>,
+                        },
+                    ]
+                }
+            ]
+        }
+        ]);
+
+
+    return <RouterProvider router={browserRouter} future={{ v7_startTransition: true }} />;
+}
+
+export default CustomRouterProvider
