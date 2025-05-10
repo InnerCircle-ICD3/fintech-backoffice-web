79ab9aadef1f9c87b8f11820f546fc4b903c380e
BORA <a52447879@gmail.com>
2025-05-11 00:38:15 +0900

    fix: 테스트 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+/// <reference types="vitest" />
 import * as path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // 핵심 설정: @ -> src
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.tsx',
+    include: ['src/**/__tests__/**/*.[jt]s?(x)'],
+    coverage: {
+      provider: 'v8',
+      exclude: ['tests/**', '**/*.d.ts', '**/*.config.[jt]s', 'src/main.tsx', 'src/routes/*'],
     },
   },
 });
