ab0f834162dafd896729c04c80da0bc37c03d01c
BORA <a52447879@gmail.com>
2025-05-19 19:56:12 +0900

    feat: 리프레시 토큰 인터셉터 설정

--- src/services/api-instance.ts
+++ src/services/api-instance.ts
@@ -1,126 +1,20 @@
-import { endPoint } from '@/api/auth/auth-endpoint';
-import { useAuthStore } from '@/stores/auth-store';
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import { StatusCodes } from 'http-status-codes';
-import { retryRequestManager } from './retry-request-manager';
-import { authApi } from '@/api/auth/auth-api';
-import { ReissueResponseType } from '@/api/auth/auth-schema';
+import axios from 'axios';
+import { setupInterceptors } from './axios-interceptors';
 
-const TIME_OUT = 5000; // 5초
+export const TIME_OUT = 5000; // 5초
 
-export const axiosInstance = axios.create({
+const baseConfig = {
   baseURL: import.meta.env.VITE_API_URL,
   timeout: TIME_OUT,
-});
-
-/** 인터셉터 ID 변수 */
-let requestInterceptorId: number;
-let responseInterceptorId: number;
-
-interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-  _retry?: boolean;
-}
-
-const retry = retryRequestManager({
-  cleanupTimeOut: TIME_OUT,
-});
-
-/** 인터셉터 초기화 함수
- * @description 토큰이 변경될 때 기존 인터셉터를 제거하고 새 인터셉터를 설정하기 위해 사용됩니다.
- */
-const initializeInterceptors = () => {
-  if (requestInterceptorId !== undefined) {
-    axiosInstance.interceptors.request.eject(requestInterceptorId);
-  }
-  if (responseInterceptorId !== undefined) {
-    axiosInstance.interceptors.response.eject(responseInterceptorId);
-  }
 };
 
-/** 인터셉터 설정 함수*/
-const setupInterceptors = (accessToken: string | null) => {
-  requestInterceptorId = axiosInstance.interceptors.request.use(
-    (config: InternalAxiosRequestConfig) => {
-      const isTokenReissueRequest = config.url?.includes(endPoint.reissue);
+export const axiosInstance = axios.create(baseConfig);
 
-      const tokenToUse = isTokenReissueRequest //
-        ? useAuthStore.getState().refreshToken
-        : accessToken;
+export const axiosInstanceWithoutAccessToken = axios.create(baseConfig);
 
-      if (tokenToUse) {
-        config.headers.Authorization = `Bearer ${tokenToUse}`;
-      }
+setupInterceptors(axiosInstance);
 
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-
-  responseInterceptorId = axiosInstance.interceptors.response.use(
-    (response) => response.data,
-    async (error: AxiosError) => {
-      if (!error.response?.status || !error.config) {
-        return Promise.reject(error);
-      }
-
-      const { status } = error.response;
-      const originalRequest = error.config as CustomAxiosRequestConfig;
-
-      const isUnauthorizedError = status === StatusCodes.UNAUTHORIZED;
-      const isRetryAllowed = !originalRequest._retry;
-
-      if (isUnauthorizedError && isRetryAllowed) {
-        originalRequest._retry = true;
-
-        try {
-          let newTokenInfo: ReissueResponseType;
-
-          return await retry({
-            getToken: async () => {
-              try {
-                const response = await authApi.reissue();
-                newTokenInfo = {
-                  accessToken: response.accessToken,
-                  refreshToken: response.refreshToken,
-                };
-                return response.accessToken;
-              } catch (error) {
-                useAuthStore.getState().clearTokens();
-                throw error;
-              }
-            },
-            onRefetch: async (token) => {
-              originalRequest.headers.Authorization = `Bearer ${token}`;
-              return axiosInstance(originalRequest);
-            },
-            onError: async (error) => {
-              return Promise.reject(error);
-            },
-            onComplete: () => {
-              useAuthStore.getState().setTokens(newTokenInfo);
-            },
-          });
-        } catch (error) {
-          return Promise.reject(error);
-        }
-      }
-      return Promise.reject(error);
-    }
-  );
-};
-
-/** 초기 인터셉터 설정
- * @description 파일이 처음 로드될 때 현재 토큰으로 인터셉터를 설정합니다.
- */
-setupInterceptors(useAuthStore.getState().accessToken);
-
-/** 토큰 변경 구독
- * @description 토큰이 변경될 때마다 인터셉터를 초기화하고 새로 설정합니다.
- */
-useAuthStore.subscribe(
-  (state) => state.accessToken,
-  (accessToken) => {
-    initializeInterceptors();
-    setupInterceptors(accessToken);
-  }
+axiosInstanceWithoutAccessToken.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
 );
---
+++ src/services/axios-interceptors.ts
@@ -0,0 +1,35 @@
+import { StatusCodes } from 'http-status-codes';
+import { requestRefresh } from './request-refresh';
+import { useAuthStore } from '@/stores/auth-store';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+
+/**
+ * 인터셉터 설정 함수
+ * */
+export const setupInterceptors = (axiosInstance: AxiosInstance) => {
+  axiosInstance.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const accessToken = useAuthStore.getState().accessToken;
+
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response) => response.data,
+    (error) => Promise.reject(error)
+  );
+
+  /** 리프레시 토큰 인터셉터 */
+  createAuthRefreshInterceptor(axiosInstance, requestRefresh, {
+    statusCodes: [StatusCodes.UNAUTHORIZED],
+    pauseInstanceWhileRefreshing: true,
+    interceptNetworkError: true,
+  });
+};
