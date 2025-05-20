a68319f76425f538875ab562286f113ee56de0b2
BORA <a52447879@gmail.com>
2025-05-20 20:29:49 +0900

    feat: 커스텀 영속성 저장소 추가 및 rememberMe 상태 관리 개선

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -1,6 +1,12 @@
 import { produce } from 'immer';
 import { create } from 'zustand';
-import { devtools, persist, subscribeWithSelector } from 'zustand/middleware';
+import {
+  createJSONStorage,
+  devtools,
+  persist,
+  StateStorage,
+  subscribeWithSelector,
+} from 'zustand/middleware';
 
 const STORAGE_KEY = 'auth-store';
 
@@ -13,10 +19,50 @@ interface AuthState {
   isHydrated: boolean /** 타이밍 이슈 해결을 위한 hydrated 상태 */;
   accessToken: string | null;
   refreshToken: string | null;
-  setTokens: ({ accessToken, refreshToken }: TokenType) => void;
+  rememberMe: boolean;
+  setTokens: ({ accessToken, refreshToken }: TokenType, rememberMe: boolean) => void;
   setIsHydrated: () => void;
   clearTokens: () => void;
 }
+
+const getCustomPersistentStorage = (): StateStorage => {
+  return {
+    getItem: (name: string): string | Promise<string | null> | null => {
+      const localData = localStorage.getItem(name);
+      if (localData) {
+        return localData;
+      }
+
+      const sessionData = sessionStorage.getItem(name);
+      if (sessionData) {
+        return sessionData;
+      }
+      return null;
+    },
+    setItem: (name: string, value: string): void | Promise<void> => {
+      try {
+        const { state: persistedState } = JSON.parse(value) as {
+          state: Pick<AuthState, 'rememberMe'>;
+        };
+
+        if (persistedState.rememberMe) {
+          localStorage.setItem(name, value);
+          sessionStorage.removeItem(name);
+        } else {
+          sessionStorage.setItem(name, value);
+          localStorage.removeItem(name);
+        }
+      } catch (e) {
+        console.error('customStorage setItem 오류:', e);
+        localStorage.setItem(name, value);
+      }
+    },
+    removeItem: (name: string): void | Promise<void> => {
+      localStorage.removeItem(name);
+      sessionStorage.removeItem(name);
+    },
+  };
+};
 
 export const useAuthStore = create<AuthState>()(
   devtools(
@@ -26,11 +72,13 @@ export const useAuthStore = create<AuthState>()(
           isHydrated: false,
           accessToken: null,
           refreshToken: null,
-          setTokens: ({ accessToken, refreshToken }: TokenType) =>
+          rememberMe: true,
+          setTokens: ({ accessToken, refreshToken }: TokenType, rememberMe: boolean) =>
             set(
               produce((state) => {
                 state.accessToken = accessToken;
                 state.refreshToken = refreshToken;
+                state.rememberMe = rememberMe;
               }),
               false,
               'auth/setTokens'
@@ -56,10 +104,12 @@ export const useAuthStore = create<AuthState>()(
         }),
         {
           name: STORAGE_KEY,
+          storage: createJSONStorage(getCustomPersistentStorage),
           partialize: (state) => ({
             accessToken: state.accessToken,
             refreshToken: state.refreshToken,
             isHydrated: state.isHydrated,
+            rememberMe: state.rememberMe,
           }),
           onRehydrateStorage: () => (state) => {
             if (state) {
