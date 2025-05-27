46466d50da6154863295efba08ffac3eca434fbd
BORA <a52447879@gmail.com>
2025-05-27 23:31:05 +0900

    feat: 가맹점 정보 쿼리 및 상수 추가

---
+++ src/constants/queries.ts
@@ -0,0 +1,5 @@
+export const QUERY_KEYS = {
+  MERCHANT: {
+    INFO: ['merchant', 'info'] as const,
+  },
+} as const;
---
+++ src/queries/merchants/queries.ts
@@ -0,0 +1,19 @@
+import { merchantsApi } from '@/api/merchants/info/api';
+import { QUERY_KEYS } from '@/constants/queries';
+import { QueryClient } from '@tanstack/react-query';
+import { LoaderFunction } from 'react-router-dom';
+import { formatMerchantInfo } from './format';
+
+/** queryClient.ensureQueryData는 select 옵션을 실행하지 않는 문제가 있음 */
+const merchantInfoQuery = {
+  queryKey: QUERY_KEYS.MERCHANT.INFO,
+  queryFn: async () => {
+    const response = await merchantsApi.getInfo();
+    return formatMerchantInfo(response);
+  },
+};
+export const merchantLoader =
+  (queryClient: QueryClient): LoaderFunction =>
+  async () => {
+    return queryClient.ensureQueryData(merchantInfoQuery);
+  };
