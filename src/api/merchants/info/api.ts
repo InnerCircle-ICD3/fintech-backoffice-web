77d87ccb1b03e5696a22b924cc8cbef040fb0833
BORA <a52447879@gmail.com>
2025-05-27 23:30:01 +0900

    feat: 가맹점 정보 API 및 스키마 추가

---
+++ src/api/merchants/info/api.ts
@@ -0,0 +1,14 @@
+import { createApiEndpoint } from '@/services/api-factory';
+import { MerchantInfoResponseSchema, MerchantInfoResponseType } from './schema';
+
+/**
+ * 가맹점 API 엔드포인트
+ */
+export const merchantsApi = {
+  getInfo: createApiEndpoint<undefined, MerchantInfoResponseType>({
+    path: '/merchants/info',
+    method: 'get',
+    requestSchema: undefined,
+    responseSchema: MerchantInfoResponseSchema,
+  }),
+};
