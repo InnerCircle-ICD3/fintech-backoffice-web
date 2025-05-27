69d4d11f3059dddf154c36444a0e06caeb56f500
BORA <a52447879@gmail.com>
2025-05-27 23:53:20 +0900

    feat: SDK 관련 핸들러

--- src/tests/mocks/fixtures/index.ts
+++ src/tests/mocks/fixtures/index.ts
@@ -1,7 +1,11 @@
 import { loginResponse } from './auth/login';
 import exampleLoader from './example-loader';
+import { merchantsInfoResponse } from './merchants/info';
+import { issueResponse } from './sdk/issue';
 
 export default {
   exampleLoader,
   loginResponse,
+  issueResponse,
+  merchantsInfoResponse,
 };
---
+++ src/tests/mocks/fixtures/sdk/issue.ts
@@ -0,0 +1,4 @@
+export const issueResponse = {
+  sdkKey: 'sdk_1234-uuid',
+  expiresAt: '2025-11-12T15:30:00',
+};
