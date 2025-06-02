bf011aac7a513f79c97259a53014ef2dd9742228
BORA <a52447879@gmail.com>
2025-06-02 21:58:34 +0900

    refactor: Axios 인스턴스 생성 방식 변경 및 apiInstance 통일 (#37)

--- src/services/request-refresh.ts
+++ src/services/request-refresh.ts
@@ -1,7 +1,7 @@
 import { RefreshTokenFailedError } from '@/services/api-error';
 import { useAuthStore } from '@/stores/auth';
 import type { AxiosError } from 'axios';
-import { axiosInstance } from './api-instance';
+import { mainApiInstance } from './api-instance';
 
 export const requestRefresh = async (failedRequest: AxiosError) => {
   try {
@@ -16,7 +16,7 @@ export const requestRefresh = async (failedRequest: AxiosError) => {
 
     console.log('Attempting token refresh...');
 
-    const { data: newToken } = await axiosInstance({
+    const { data: newToken } = await mainApiInstance({
       url: '/auth/reissue',
       method: 'POST',
       headers: {
