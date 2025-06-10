8c4dc02983cd39dec236458ce15d550cace2594e
BORA <a52447879@gmail.com>
2025-06-10 16:26:57 +0900

    feat: 거래내역 리스트 컴포넌트 리팩토링 및 Suspense를 활용한 데이터 로딩 처리 추가

--- src/features/transaction/transaction-list/index.tsx
+++ src/features/transaction/transaction-list/index.tsx
@@ -1,11 +1,12 @@
 import { transactionApi } from '@/api/transaction/api';
+import Flex from '@/components/layout/flex';
 import AdminSection from '@/components/layout/section/admin';
 import { QUERY_KEYS } from '@/constants/queries';
-
 import { MerchantInfoType } from '@/queries';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { Suspense } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import SearchFilter from './components/SearchFilter ';
+import SearchFilter from './components/filters/SearchFilter';
 import Table from './components/Table';
 import { useTransactionParams } from './hooks/useTransactionParams';
 import { selectFormattedTransactions } from './selectors';
@@ -14,13 +15,7 @@ const TransactionList = () => {
   const merchantInfo = useOutletContext<MerchantInfoType>();
   const { params } = useTransactionParams();
 
-  const {
-    data: rawTransactions,
-    isLoading,
-    isFetching,
-    error,
-    refetch,
-  } = useQuery({
+  const { data: transactions, isFetching } = useSuspenseQuery({
     queryKey: [
       QUERY_KEYS.TRANSACTION.ALL,
       params.page,
@@ -35,27 +30,20 @@ const TransactionList = () => {
         ...params,
       }),
     select: selectFormattedTransactions,
-    staleTime: 0,
-    gcTime: 0,
   });
-
-  if (error) {
-    return (
-      <div>
-        잠시 후 다시 시도해주세요.
-        <button onClick={() => refetch()}>다시 시도</button>
-      </div>
-    );
-  }
-
-  if (!rawTransactions) {
-    return <div>No data</div>;
-  }
 
   return (
     <AdminSection label={'거래 내역'}>
-      <SearchFilter />
-      <Table data={rawTransactions} isFetching={isFetching} isPending={isLoading} />
+      <SearchFilter>
+        <Flex gap={'16px'}>
+          <SearchFilter.Status />
+          <SearchFilter.DateRange />
+        </Flex>
+        <SearchFilter.Actions />
+      </SearchFilter>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Table data={transactions} isFetching={isFetching} />
+      </Suspense>
     </AdminSection>
   );
 };
