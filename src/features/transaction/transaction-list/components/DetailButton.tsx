44c1da47c22229b8cc457efc0f887403c1ec4fc1
BORA <a52447879@gmail.com>
2025-06-10 16:26:49 +0900

    feat: 거래내역 메타데이터를 상수에서 직접 정의하여 의존성 제거

--- src/features/transaction/transaction-list/components/DetailButton.tsx
+++ src/features/transaction/transaction-list/components/DetailButton.tsx
@@ -9,7 +9,6 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { GridTable } from '@/components/ui/table';
-import { transactionDetailMetaData } from '@/constants/transaction-mock';
 import { Row } from '@tanstack/react-table';
 import { FormattedTransaction } from '../selectors';
 
@@ -31,7 +30,30 @@ const DetailButton = ({ row }: DetailButtonProps) => {
         </DialogHeader>
         <DialogDescription>
           <Flex gap={'6px'} direction={'column'}>
-            <GridTable data={row?.original} metaData={transactionDetailMetaData} />
+            <GridTable
+              data={row?.original}
+              metaData={{
+                date: {
+                  label: '승인일자',
+                },
+                time: {
+                  label: '승인시간',
+                },
+                cardCompany: {
+                  label: '카드사',
+                },
+                cardNumber: {
+                  label: '카드번호',
+                },
+                paidAmount: {
+                  label: '승인금액',
+                  format: 'currency',
+                },
+                paymentStatus: {
+                  label: '거래상태',
+                },
+              }}
+            />
           </Flex>
         </DialogDescription>
       </DialogContent>
