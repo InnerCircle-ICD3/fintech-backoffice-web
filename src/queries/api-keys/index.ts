132b0f947c525921d52b11caf072009e76beb2bc
BORA <a52447879@gmail.com>
2025-06-18 22:34:37 +0900

    refactor: SDK 키 쿼리 옵션을 별도 파일로 분리 및 불필요한 코드 제거

--- src/queries/api-keys/index.ts
+++ src/queries/api-keys/index.ts
@@ -1,13 +1,6 @@
 import { apiKeysApi } from '@/api/api-keys/api';
-import { sdkApi } from '@/api/sdk/api';
 import { QUERY_KEYS } from '@/constants/queries';
 import { queryOptions } from '@tanstack/react-query';
-
-export const sdkKeyQueryOptions = () =>
-  queryOptions({
-    queryKey: QUERY_KEYS.SDK.KEY,
-    queryFn: () => sdkApi.get(),
-  });
 
 export const apiKeyQueryOptions = (userId: number) =>
   queryOptions({
